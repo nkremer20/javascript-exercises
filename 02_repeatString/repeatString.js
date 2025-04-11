4502562c49c68d8356ae267cfb2f1cdfa48b2b55
Noah Kremer <75767608+nkremer20@users.noreply.github.com>
2025-04-11 17:59:24 -0500

    Completed 02_repeatString

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,14 @@
-const repeatString = function() {
+const repeatString = function(word, num) {
+    if (num < 0) {
+        return "ERROR"
+    }
 
+    let output = "";
+    for (let i = 0; i < num; i++) {
+        output += word
+    }
+
+    return output;
 };
 
 // Do not edit below this line
