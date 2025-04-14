f46d3f2a02d98e25040fac060af0ea15a9320c43
Noah Kremer <75767608+nkremer20@users.noreply.github.com>
2025-04-13 21:21:41 -0500

    Completed 04_removeFromArray

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,16 @@
-const removeFromArray = function() {
+const removeFromArray = function(inArray, ...removeItems) {
+    for (let i = 0; i < removeItems.length; i++) {
+        let index = 0; 
+        while (index < inArray.length) {
+            if (inArray[index] === removeItems[i]) {
+                inArray.splice(index, 1);
+            } else {
+                index++;
+            }
+        }
+    }
+
+    return inArray;
 };
 
 // Do not edit below this line
