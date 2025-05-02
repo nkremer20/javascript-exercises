14388404d6dc354fdf6236a971f5a0bd534552e7
Noah Kremer <75767608+nkremer20@users.noreply.github.com>
2025-05-02 15:27:50 -0500

    Completed 08_calculator

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,36 @@
-const add = function() {
-	
+const add = function(num1, num2) {
+	return num1 + num2;
 };
 
-const subtract = function() {
-	
+const subtract = function(num1, num2) {
+	return num1 - num2;
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+	const total = arr.reduce((sum, item) => sum + item, 0);
+  return total;
 };
 
-const multiply = function() {
-
+const multiply = function(arr) {
+  total = arr.reduce((multTotal, item) => multTotal * item, 1);
+  return total;
 };
 
-const power = function() {
-	
+const power = function(num1, num2) {
+	return num1 ** num2;
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+	if (num === 0) {
+    return 1;
+  } else {
+    const numArray = [];
+    for (num; num > 0; num--) {
+      numArray.push(num);
+    }
+    const factorial = numArray.reduce((total, item) => total * item, 1);
+    return factorial
+  }
 };
 
 // Do not edit below this line
