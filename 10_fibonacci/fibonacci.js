261799efdc1ecdc79f46afe73256c78c90cd64ef
Noah Kremer <75767608+nkremer20@users.noreply.github.com>
2025-05-03 00:56:45 -0500

    Completed 10_fibonacci

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,24 @@
-const fibonacci = function() {
+const fibonacci = function(num) {
+    num = parseInt(num);
+    if (num >= 0 && num <= 1) {
+        return num;
+    } else if (num < 0) return 'OOPS';
 
+    let current = 0;
+
+    let prev1 = 1;
+    let prev2 = 0;
+
+    for (let i=2; i <= num; i++) {
+        current = prev1 + prev2;
+
+        prev2 = prev1;
+
+        prev1 = current;
+
+    };
+
+    return current;
 };
 
 // Do not edit below this line
