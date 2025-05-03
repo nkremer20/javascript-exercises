963ed7eb1f079a47e183ed5519fa0fb46729a4ce
Noah Kremer <75767608+nkremer20@users.noreply.github.com>
2025-05-03 00:28:20 -0500

    Completed 09_palindromes

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,18 @@
-const palindromes = function () {
+const palindromes = function (str) {
+    const strFormatted = str.toLowerCase().replace(/[!"#$%&'()*+,-./:;<=>?@[\]^_`{|}~]/g, '').replace(/\s+/g, "");
+    const strReverse = strFormatted.split('').reverse().join('');
 
+    console.log(strFormatted);
+    console.log(strReverse);
+
+    if (strFormatted === strReverse) {
+        return true;
+    } else {
+        return false;
+    }
 };
+
+palindromes('A car, a man, a maraca.')
 
 // Do not edit below this line
 module.exports = palindromes;
