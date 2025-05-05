adf4615b189caa162b97e3fe7bb392a8f34aaff2
Noah Kremer <75767608+nkremer20@users.noreply.github.com>
2025-05-04 22:09:22 -0500

    Completed 12_findTheOldest

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,24 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
+    people.sort((a,b) => {
+        let aAge;
+        let bAge;
 
+        if (!a.yearOfDeath) {
+            aAge = ((new Date()).getFullYear()) - a.yearOfBirth;
+        } else {
+            aAge = a.yearOfDeath - a.yearOfBirth;
+        }
+
+        if (!b.yearOfDeath) {
+            bAge = ((new Date()).getFullYear()) - b.yearOfBirth;
+        } else {
+            bAge = b.yearOfDeath - b.yearOfBirth;
+        }
+
+        return bAge - aAge;
+    })
+
+    return people[0];
 };
 
 // Do not edit below this line
